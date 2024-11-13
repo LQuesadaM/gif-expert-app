854acc0ac6744594c8935d1d88fbd2d952749c6b
LQuesadaM <luisquesada163@gmail.com>
2024-11-12 22:28:44 -0300

    init

---
+++ src/GifExpertApp.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+
+export const GifExpertApp = () => {
+	const [categories, setCategories] = useState(['One Puch'])
+
+	return (
+		<>
+			<h1 className="text-3xl underline">GifExpertApp</h1>
+			<input type="text" name="" id="" />
+		</>
+	)
+}
